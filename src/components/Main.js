a766ba313af846621e5a518b6cc4ee09729e89c8
OnionPowder01 <autonatione36@gmail.com>
2023-01-10 15:47:26 +0200

    main set-up

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Main(props) {
     return (
         <main className={props.darkMode ? "dark" : ""}>
-            <h1 className="fun-facts">Fun facts about React</h1>
-            <ul className="react-list">
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
