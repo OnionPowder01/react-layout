0b645a49d1337a74e17b2b6e6a40bb1edac0c688
OnionPowder01 <autonatione36@gmail.com>
2023-01-10 15:47:32 +0200

    nav setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ import myImage from '../images/React-icon.svg.png'
 export default function Navbar(props) {
     return (
         <nav className={props.darkMode ? "dark" : ""}>
-            <img src={myImage} alt="react-logo" className="react-logo" />
-        <h3 className="react-h3">ReactFacts</h3>
+            <img src={myImage} alt="react-logo" className="nav--logo_icon" />
+        <h3 className="nav--logo_text">ReactFacts</h3>
         <div className="toggler">
             <p className="toggler--light">Light</p>
             <div className="toggler--slider" onClick={props.toggleDarkMode}>
