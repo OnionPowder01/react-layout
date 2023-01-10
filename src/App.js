b1a4d7832d84522a62409685624afafabb0c3e02
OnionPowder01 <autonatione36@gmail.com>
2023-01-10 15:14:40 +0200

    Setup for light&dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Main from "./components/Main";
 import Navbar from "./components/NavBar";
 import "./App.css";
+import React, { useState } from "react";
 
 export default function App() {
+   const [darkMode, setDarkMode]= useState(true)
+
+   function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+   }
+
+    
+
+
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar 
+      darkMode={darkMode}
+      toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
